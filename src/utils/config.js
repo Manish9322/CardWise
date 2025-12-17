278fe4512dc9b5726e238f8a6a5c1de695463650
Manish Sonawane <manishsonawane3010@gmail.com>
2025-12-17 18:53:02 +0000

    create a db.js file in utils folder, write following code in that file "

---
+++ src/utils/config.js
@@ -0,0 +1,2 @@
+// In a real application, you would get this from environment variables
+export const MONGODB_URL = "your-mongodb-connection-string-here";
---
+++ src/utils/db.js
@@ -0,0 +1,16 @@
+// db.js
+import mongoose from "mongoose";
+import { MONGODB_URL } from "./config.js";
+
+export const _db = async () => {
+  try {
+    const conn = await mongoose.connect(MONGODB_URL);
+
+    console.log("MongoDB connected successfully");
+    return conn;
+  } catch (error) {
+    console.error("MongoDB connection failed");
+    console.error(error.message);
+    throw error;
+  }
+};
