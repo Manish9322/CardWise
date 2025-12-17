4a7b20a2b882234fe862cf550c7f257c75cdd0c0
Manish Sonawane <manishsonawane3010@gmail.com>
2025-12-17 20:14:44 +0000

    create following state UIs for manage questions, manage users, questions

--- src/utils/services/api.js
+++ src/utils/services/api.js
@@ -50,16 +50,80 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 export const api = createApi({
   reducerPath: "api",
   baseQuery: baseQueryWithReauth,
-  tagTypes: ["DB"],
+  tagTypes: ["DB", "Questions", "Users"],
   endpoints: (builder) => ({
     // DB connection test endpoint
     testDBConnection: builder.query({
       query: () => "/test-db-connection",
       providesTags: ["DB"],
     }),
+    // Questions endpoints
+    getQuestions: builder.query({
+      query: () => "/questions",
+      providesTags: ["Questions"],
+    }),
+    addQuestion: builder.mutation({
+      query: (question) => ({
+        url: "/questions",
+        method: "POST",
+        body: question,
+      }),
+      invalidatesTags: ["Questions"],
+    }),
+    updateQuestion: builder.mutation({
+      query: ({ id, ...question }) => ({
+        url: `/questions/${id}`,
+        method: "PUT",
+        body: question,
+      }),
+      invalidatesTags: ["Questions"],
+    }),
+    deleteQuestion: builder.mutation({
+      query: (id) => ({
+        url: `/questions/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Questions"],
+    }),
+    // Users endpoints
+    getUsers: builder.query({
+      query: () => "/users",
+      providesTags: ["Users"],
+    }),
+    addUser: builder.mutation({
+      query: (user) => ({
+        url: "/users",
+        method: "POST",
+        body: user,
+      }),
+      invalidatesTags: ["Users"],
+    }),
+    updateUser: builder.mutation({
+      query: ({ id, ...user }) => ({
+        url: `/users/${id}`,
+        method: "PUT",
+        body: user,
+      }),
+      invalidatesTags: ["Users"],
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Users"],
+    }),
   }),
 });
 
 export const {
   useTestDBConnectionQuery,
+  useGetQuestionsQuery,
+  useAddQuestionMutation,
+  useUpdateQuestionMutation,
+  useDeleteQuestionMutation,
+  useGetUsersQuery,
+  useAddUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
 } = api;
