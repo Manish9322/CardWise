d9063491f3e55c88bf292caa34ccb33988e2ffd6
Shubham Vanarse <100429473+shubs03@users.noreply.github.com>
2025-12-19 16:28:17 +0530

    Imp : line height changed

--- src/components/game/GuessCard.tsx
+++ src/components/game/GuessCard.tsx
@@ -11,7 +11,7 @@ export default function GuessCard({ question, answer, isFlipped }: GuessCardProp
     <div className="relative h-96 w-full max-w-7xl flex items-center justify-center">
       <div
         className={cn(
-          'flex items-center justify-center p-6 text-5xl md:text-7xl font-semibold text-center leading-snug transition-opacity duration-300 ease-in-out',
+          'flex items-center justify-center p-6 text-5xl md:text-7xl font-semibold text-center leading-normal transition-opacity duration-300 ease-in-out',
           isFlipped ? 'opacity-0' : 'opacity-100'
         )}
       >
@@ -19,7 +19,7 @@ export default function GuessCard({ question, answer, isFlipped }: GuessCardProp
       </div>
       <div
         className={cn(
-          'absolute inset-0 flex items-center justify-center p-6 text-5xl md:text-7xl font-bold text-primary text-center leading-snug transition-opacity duration-300 ease-in-out',
+          'absolute inset-0 flex items-center justify-center p-6 text-5xl md:text-7xl font-bold text-primary text-center leading-normal transition-opacity duration-300 ease-in-out',
           isFlipped ? 'opacity-100' : 'opacity-0'
         )}
       >
