bd8d6850ba29b5df0dac3d4cdb992f9d33c56e4b
Manish Sonawane <manishsonawane3010@gmail.com>
2025-12-22 16:36:21 +0000

    ok so i want to remove that ready made favicon that we have in the tab's

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="data:," />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
